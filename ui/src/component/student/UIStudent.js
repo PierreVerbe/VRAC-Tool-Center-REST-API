b5d304bc48496032465ea71439496f641e4a397c
Pierre Verbe <pierre.verbe@free.fr>
2021-08-03 23:46:59 +0200

    Connected React to Rest API

--- ui/src/component/student/UIStudent.js
+++ ui/src/component/student/UIStudent.js
@@ -11,10 +11,12 @@ const UIStudent = ({ students, getStudents, insertStudent }) => {
     useEffect(getStudents, [])
 
     const onSubmit = (event) => {
-        const { nickname, legalAge } = event.target.elements
-        insertStudent({ nickname: nickname.value, legalAge: legalAge.value })
-        nickname.value = null
-        legalAge.value = null
+        const { id, name, age, grade } = event.target.elements
+        insertStudent({ id: id.value, name: name.value, age: age.value, grade: grade.value })
+        id.value = null
+        name.value = null
+        age.value = null
+        grade.value = null
         event.preventDefault()
     }
 
@@ -22,9 +24,11 @@ const UIStudent = ({ students, getStudents, insertStudent }) => {
         <div className="marginWrapper">
             <div className="jobInput">
                 <form onSubmit={onSubmit}>
-                    <input id="example" type="text" name="text"></input>
-                    <input id="example" type="text" name="text"></input>
-                    <input type="submit" value="Create merc" />
+                    <input id="example" type="number" name="id"></input>
+                    <input id="example" type="text" name="name"></input>
+                    <input id="example" type="number" step="any" name="age"></input>
+                    <input id="example" type="number" name="grade"></input>
+                    <input type="submit" value="Created student" />
                 </form>
             </div>
             <ListStudent students={students} />
