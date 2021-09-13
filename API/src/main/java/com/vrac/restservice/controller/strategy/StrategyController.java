0dafbe8163ee14afb2a502eb7b9fbec6d1a166c1
Pierre Verbe <pierre.verbe@free.fr>
2021-09-13 23:19:29 +0200

    added delete strategy

--- API/src/main/java/com/vrac/restservice/controller/strategy/StrategyController.java
+++ API/src/main/java/com/vrac/restservice/controller/strategy/StrategyController.java
@@ -4,13 +4,11 @@ import com.vrac.restservice.model.entity.strategy.Strategy;
 import com.vrac.restservice.repository.StrategyRepository;
 import com.vrac.restservice.service.SequenceGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static com.vrac.restservice.model.entity.strategy.Strategy.SEQUENCE_NAME;
 
@@ -23,12 +21,12 @@ public class StrategyController {
     @Autowired
     private SequenceGeneratorService service;
 
-    @GetMapping(value = "/findAllStrategies")
+    @GetMapping(value = "/find/allStrategies")
     public List<Strategy> getAllStrategies() {
         return strategyRepository.findAll();
     }
 
-    @PostMapping(value = "/insertStrategy")
+    @PostMapping(value = "/insert/strategy")
     public String createStrategy(@RequestBody Strategy strategy) {
         // Generate LocalDateTime
         LocalDateTime currentTime = LocalDateTime.now();
@@ -39,6 +37,19 @@ public class StrategyController {
 
         Strategy insertedStrategy = strategyRepository.insert(strategy);
         return String.format("Strategy id=%d created", insertedStrategy.getId());
+    }
+
+    @DeleteMapping("/delete/strategy")
+    public String deleteStrategyById(@RequestBody Long id) {
+        Optional<Strategy> strategy = strategyRepository.findById(id);
+
+        if (strategy.isPresent()) {
+            strategyRepository.deleteById(id);
+            return String.format("Strategy id=%d deleted", id);
+        }
+        else {
+            return String.format("Strategy id=%d does not exist", id);
+        }
     }
 
 }
