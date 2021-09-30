7f9ea25b7e62f7dc3072bd16835a5b2e3359edbd
Pierre Verbe <pierre.verbe@free.fr>
2021-09-30 17:42:44 +0200

    Added Base monitoring

--- API/src/main/java/com/vrac/restservice/entity/MongoCollection.java
+++ API/src/main/java/com/vrac/restservice/entity/MongoCollection.java
@@ -3,4 +3,5 @@ package com.vrac.restservice.entity;
 public interface MongoCollection {
     public static final String SEQUENCE = "Sequence";
     public static final String STRATEGY = "Strategy";
+    public static final String MONITORING = "Monitoring";
 }
