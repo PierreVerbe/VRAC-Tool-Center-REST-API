2730efc354c55abf2fdfec1edf59b7ba5bfff247
Pierre Verbe <pierre.verbe@free.fr>
2021-10-02 14:49:43 +0200

    Added tests for Strategy

--- API/src/test/java/com/vrac/restservice/unitary/service/StrategyServiceTest.java
+++ API/src/test/java/com/vrac/restservice/unitary/service/StrategyServiceTest.java
@@ -1,6 +1,7 @@
 package com.vrac.restservice.unitary.service;
 
 import com.vrac.restservice.entity.strategy.Strategy;
+import com.vrac.restservice.error.exception.ResourceNotFoundException;
 import com.vrac.restservice.service.StrategyService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,7 @@ import org.springframework.data.mongodb.core.query.Query;
 
 import static com.vrac.restservice.entity.MongoCollection.SEQUENCE;
 import static com.vrac.restservice.entity.MongoCollection.STRATEGY;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class StrategyServiceTest {
@@ -54,6 +54,7 @@ public class StrategyServiceTest {
     @Test
     public void findStrategyWithIdTest() {
         // Given
+        Long id = 1L;
         Strategy strategy = new Strategy();
         strategy.setName("myName");
         strategy.setDescription("myDescription");
@@ -62,15 +63,27 @@ public class StrategyServiceTest {
 
         // When
         strategyService.insertStrategy(strategy);
-        Strategy result = strategyService.findStrategyWithId(1L);
+        Strategy result = strategyService.findStrategyWithId(id);
 
         // Then
-        assertEquals(result.getId(), 1L);
+        assertEquals(result.getId(), id);
         assertEquals(result.getName(), "myName");
         assertEquals(result.getDescription(), "myDescription");
         assertEquals(result.getSender(), "mySender");
         assertNull(result.getStrategy());
         assertEquals(result.getVersion(), "v1.0");
+    }
+
+    @Test
+    public void findNoneExistingStrategyWithIdTest() {
+        // Given
+        Long id = 1L;
+
+        // When
+        Throwable exception = assertThrows(ResourceNotFoundException.class, () -> strategyService.findStrategyWithId(id));
+
+        // Then
+        assertEquals("Strategy not found with id:1", exception.getMessage());
     }
 
     @Test
@@ -80,7 +93,15 @@ public class StrategyServiceTest {
 
     @Test
     public void updateNoneExitingStrategyTest() {
+        // Given
+        Strategy strategy = new Strategy();
+        strategy.setId(999L);
 
+        // When
+        Throwable exception = assertThrows(ResourceNotFoundException.class, () -> strategyService.updateStrategy(strategy));
+
+        // Then
+        assertEquals("Strategy not found with id:999", exception.getMessage());
     }
 
     @Test
@@ -94,10 +115,10 @@ public class StrategyServiceTest {
         Long id = 999L;
 
         // When
-        String result = strategyService.deleteStrategy(id);
+        Throwable exception = assertThrows(ResourceNotFoundException.class, () -> strategyService.deleteStrategy(id));
 
         // Then
-        assertEquals(result, "Strategy id=999 does not exist");
+        assertEquals("Strategy not found with id:999", exception.getMessage());
     }
 
 }
